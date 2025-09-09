5df6a700f2cf5faa6794fa71c50a54959c9dd48f
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-09 11:44:00 -0300

    Feat: Enum responsible for the status of the order, having Pending Payment, Processing, Shipped and Ordered.

---
+++ Entities/EntitiesEnum/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderSystem.Entities.EntitiesEnum
+{
+    internal enum OrderStatus : int
+    {
+        Pending_Payment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Ordered = 3
+    }
+}
