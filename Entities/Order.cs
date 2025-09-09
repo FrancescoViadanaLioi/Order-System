a456b2271024c4a202d76341bacafc5c51067433
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-09 12:14:22 -0300

    Feat: Constructor of the Order Entity.

---
+++ Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OrderSystem.Entities.EntitiesEnum;
+
+namespace OrderSystem.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; private set; }
+        public OrderStatus Status { get; private set; }
+
+        public Order(DateTime moment, OrderStatus status)
+        {
+            Moment = moment;
+            Status = status;
+        }
+    }
+}
