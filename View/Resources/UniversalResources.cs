e461d2870c03abc8df525d78f0e2a1c179cb020d
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-10 11:44:00 -0300

    Feat: Universal Resources that are used in various ways in the program.

---
+++ View/Resources/UniversalResources.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.View.Resources
+{
+    internal static class DataInput
+    {
+        public static string? AskForInput(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+    }
+    internal static class Formatter
+    {
+        public static string Format(double value)
+        {
+            return value.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
