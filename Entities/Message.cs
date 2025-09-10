c44f617fa169a4435a9ffdef417c90e67ca4783c
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-10 11:43:04 -0300

    Feat: Addition of the Entity Message, which carries attributes of a message. Also, there's an enum whose types are fixed and strict.

---
+++ Entities/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OrderSystem.Entities.EntitiesEnum;
+
+namespace OrderSystem.Entities
+{
+    internal class Message
+    {
+        public string Text { get; set; }
+        public MessageKind Kind { get; set; }
+        public bool WaitForInput { get; set; }
+        public bool ClearScreen { get; set; }
+
+        public Message(string text, MessageKind kind, bool waitForInput, bool clearScreen)
+        {
+            Text = text;
+            Kind = kind;
+            WaitForInput = waitForInput;
+            ClearScreen = clearScreen;
+        }
+    }
+}
