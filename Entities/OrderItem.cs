eb72854105a1f3f057ff8b84e6321f2cec118102
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-10 11:01:13 -0300

    Feat: Order Item entity, with attributes as Quantity and Price. Also, contains a method to calculate the SubTotal.

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OrderSystem.Entities
+{
+    internal class OrderItem
+    {
+        public int Quantity { get; private set; }
+        public double Price { get; private set; }
+
+        public OrderItem(int quantity, double price)
+        {
+            if (quantity <= 0) throw new ArgumentOutOfRangeException(nameof(quantity));
+            if (price <= 0) throw new ArgumentOutOfRangeException(nameof(price));
+            Quantity = quantity;
+            Price = price;
+        }
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
