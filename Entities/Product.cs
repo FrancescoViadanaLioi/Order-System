ad94784cf1e895c6a2db76f8a99d7b6995cae9c0
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-10 11:16:02 -0300

    Feat: Now the constructor goes through validators.

---
+++ Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OrderSystem.Services.Validators;
+
+namespace OrderSystem.Entities
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Product(string name, double price)
+        {
+            if (!ProductValidator.IsValidName(name)) throw new ArgumentException("The name of the product only support characters and digits.");
+            if (!ProductValidator.IsValidPrice(price)) throw new ArgumentException("The price must be a number higher than 0.");
+            Name = name;
+            Price = price;
+        }
+    }
+}
