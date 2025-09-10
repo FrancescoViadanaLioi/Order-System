24d22df2aeafe2879c23c237f983a1aac581bd15
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-10 11:07:53 -0300

    Feat: New attribute that is a List. It is a List which type is the "OrderItem" class. Now with methods of adding and removing item.

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -7,11 +7,20 @@ namespace OrderSystem.Entities
     {
         public DateTime Moment { get; private set; }
         public OrderStatus Status { get; private set; }
+        public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
 
         public Order(DateTime moment, OrderStatus status)
         {
             Moment = moment;
             Status = status;
+        }
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
         }
     }
 }
