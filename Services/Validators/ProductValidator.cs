e3999e6bda905e7c4e52e2407002667c7d39a7f4
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-11 16:21:44 -0300

    Feat: Class responsible to validate the inputs about the product.

--- Services/Validators/ProductValidator.cs
+++ Services/Validators/ProductValidator.cs
@@ -12,9 +12,18 @@ namespace OrderSystem.Services.Validators
             name = name.Trim();
             return name.All(c => char.IsLetterOrDigit(c) || c == '-' || c == '\'' || c == ' ');
         }
+        public static bool IsValidPrice(string input)
+        {
+            return double.TryParse(input, CultureInfo.InvariantCulture, out double price) && price > 0;
+
+        }
         public static bool IsValidPrice(double price)
         {
             return price > 0;
+        }
+        public static bool IsValidQuantity(string input)
+        {
+            return int.TryParse(input, CultureInfo.InvariantCulture, out int quantity) && quantity > 0;
         }
     }
 }
