27286ccc78fde6de20590cde872124e130c3de67
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-11 17:52:10 -0300

    Feat: Addition of the missing attribute "Client". Also, a new method which converts all the information of the Order to a string value.

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text;
 using OrderSystem.Entities.EntitiesEnum;
+using OrderSystem.View.Resources;
 
 namespace OrderSystem.Entities
 {
@@ -7,11 +9,13 @@ namespace OrderSystem.Entities
     {
         public DateTime Moment { get; private set; }
         public OrderStatus Status { get; private set; }
+        public Client Client { get; private set; }
         public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
 
-        public Order(DateTime moment, OrderStatus status)
+        public Order(DateTime moment, Client client, OrderStatus status)
         {
             Moment = moment;
+            Client = client;
             Status = status;
         }
         public void AddItem(OrderItem item)
@@ -31,6 +35,22 @@ namespace OrderSystem.Entities
                 sum += item.SubTotal();
             }
             return sum;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Moment: {Moment.ToString("dd/MM/yyyy HH:mm:ss")}");
+            sb.AppendLine($"Order status: {Status}");
+            sb.AppendLine($"Client: {Client.Name} ({Client.BirthDate}) - email: {Client.Email}");
+            sb.AppendLine("Order items:");
+
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            double total = Total();
+            sb.AppendLine($"Total price: ${Formatter.Format(total)}");
+            return sb.ToString();
         }
     }
 }
