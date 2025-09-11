fdb21e87e7410c39838c3443a6de7e2bdb8c2a55
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-11 16:11:05 -0300

    Feat: Status validator method, that verifies if the status exists.

---
+++ Services/Validators/OrderValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderSystem.Entities.EntitiesEnum;
+
+namespace OrderSystem.Services.Validators
+{
+    internal class OrderValidator
+    {
+        public static bool IsValidStatus(string input)
+        {
+            return Enum.TryParse<OrderStatus>(input, ignoreCase: true, out OrderStatus status);
+        }
+    }
+}
