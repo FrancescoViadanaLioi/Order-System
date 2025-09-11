9fc0f51b4e500780a100cbc9fb84b3ff33636214
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-11 16:24:55 -0300

    Feat: Script which gets the order status by user input.

---
+++ View/OrderDataInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OrderSystem.View.Resources;
+using OrderSystem.Entities.EntitiesEnum;
+using OrderSystem.Services.Validators;
+
+namespace OrderSystem.View
+{
+    internal class OrderDataInput
+    {
+        public static OrderStatus StatusInput()
+        {
+            OrderStatus status = InputHelper.GetValidatedInput(
+                "Status: ",
+                input => !string.IsNullOrWhiteSpace(input) && OrderValidator.IsValidStatus(input),
+                input => Enum.Parse<OrderStatus>(input),
+                "This status doesn't exist in our system.");
+            return status;
+        }
+    }
+}
