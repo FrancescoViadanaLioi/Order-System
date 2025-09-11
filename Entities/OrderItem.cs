f733aedf8e1513118794a8f80418dcdc6e23e976
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-11 18:43:22 -0300

    Fix: Addition of the Product attribute to store the data about the product and also the StringBuilder for item it's here.

--- Entities/OrderItem.cs
+++ Entities/OrderItem.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Xml.Linq;
 
 namespace OrderSystem.Entities
 {
@@ -6,17 +9,27 @@ namespace OrderSystem.Entities
     {
         public int Quantity { get; private set; }
         public double Price { get; private set; }
+        public Product Product { get; private set; }
 
-        public OrderItem(int quantity, double price)
+        public OrderItem(int quantity, Product product, double price)
         {
             if (quantity <= 0) throw new ArgumentOutOfRangeException(nameof(quantity));
             if (price <= 0) throw new ArgumentOutOfRangeException(nameof(price));
             Quantity = quantity;
+            Product = product;
             Price = price;
         }
         public double SubTotal()
         {
             return Quantity * Price;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Product.Name}");
+            sb.AppendLine($"${Price.ToString("F2", CultureInfo.InvariantCulture)}");
+            sb.AppendLine($"Subtotal: ${SubTotal().ToString("F2", CultureInfo.InvariantCulture)}");
+            return sb.ToString();
         }
     }
 }
